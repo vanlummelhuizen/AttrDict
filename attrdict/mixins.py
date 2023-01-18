79cb03da76c5c0751ab73826c042d7430fedef83
Micha Hulsbosch <michahulsbosch@gmail.com>
2023-01-18 11:27:54 +0100

    fix: Import collections stuff for older and new Python versions

--- attrdict/mixins.py
+++ attrdict/mixins.py
@@ -2,7 +2,11 @@
 Mixin Classes for Attr-support.
 """
 from abc import ABCMeta, abstractmethod
-from collections import Mapping, MutableMapping, Sequence
+
+try:
+    from collections.abc import Mapping, MutableMapping, Sequence
+except ImportError:
+    from collections import Mapping, MutableMapping, Sequence
 import re
 
 import six
